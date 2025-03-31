4cf29c22049816aa6ef488db6a160c8e544450cd
Akash6222 <akashdubey25274@gmail.com>
2025-03-31 11:59:07 +0530

    Make calamres.desktop executable

--- alg_set_once.sh
+++ alg_set_once.sh
@@ -3,5 +3,8 @@
 # For setting calamares.desktop as trusted in xfce
 for f in ~/Desktop/calamares.desktop; do chmod +x "$f"; gio set -t string "$f" metadata::xfce-exe-checksum "$(sha256sum "$f" | awk '{print $1}')"; done
 
+# Make calamares.desktop executable
+chmod +x ~/Desktop/calamares.desktop
+
 # Remove the contents
 rm  ~/.config/autostart/alg_set_once.desktop ~/.config/autostart/alg_set_once.sh
