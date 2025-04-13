51b28a921a022cc5c1c71e54b82473b0bd26a0e8
Akash6222 <akashdubey25274@gmail.com>
2025-04-14 02:32:09 +0530

    Added XDG_CURRENT_DESKTOP check and some gnome settings

--- alg_set_once.sh
+++ alg_set_once.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
-# Copy calamares.desktop to Desktop
-cp /usr/share/applications/calamares.desktop /home/liveuser/Desktop/calamares.desktop
+# Check the current desktop environment
+if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
+    # Apply GNOME-specific settings
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+    gsettings set org.gnome.Console use-system-font false
+    gsettings set org.gnome.Console custom-font 'Monospace 11'
 
-# For setting calamares.desktop as trusted in xfce
-for f in ~/Desktop/calamares.desktop; do chmod +x "$f"; gio set -t string "$f" metadata::xfce-exe-checksum "$(sha256sum "$f" | awk '{print $1}')"; done
+    # Set GNOME Terminal profile font settings
+    PROFILE_ID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d "'")
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$PROFILE_ID/" use-system-font false
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$PROFILE_ID/" font 'Monospace 11'
 
-# Make calamares.desktop executable
-chmod +x ~/Desktop/calamares.desktop
+else
+    # Copy calamares.desktop to Desktop for non-GNOME environments
+    cp /usr/share/applications/calamares.desktop /home/liveuser/Desktop/calamares.desktop
 
-# Remove the contents
-rm  ~/.config/autostart/alg_set_once.desktop ~/.config/autostart/alg_set_once.sh
+    # Make calamares.desktop executable
+    chmod +x /home/liveuser/Desktop/calamares.desktop
+
+    # For XFCE, set calamares.desktop as trusted
+    if [ "$XDG_CURRENT_DESKTOP" = "XFCE" ]; then
+        for f in /home/liveuser/Desktop/calamares.desktop; do
+            gio set -t string "$f" metadata::xfce-exe-checksum "$(sha256sum "$f" | awk '{print $1}')"
+        done
+    fi
+fi
+
+# Remove the autostart files
+rm -f ~/.config/autostart/alg_set_once.desktop ~/.config/autostart/alg_set_once.sh
